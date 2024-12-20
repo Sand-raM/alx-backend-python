dd855b07e52c794e11ab53f6b305d37b51c9803c
GabieSE <138817016+GabieSE@users.noreply.github.com>
2024-12-20 17:25:58 +0200

    Implement JWT authentication with custom views and settings

--- messaging_app/chats/auth.py
+++ messaging_app/chats/auth.py
@@ -1,12 +1,12 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class CustomTokenObtainPairView(TokenObtainPairView):
     """
-        Custom Token View to add extra information like user details in response.
-        """
+    Custom Token View to add extra information like user details in response.
+    """
 
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,10 @@
-from rest_framework.permissions import  BasePermission
+from rest_framework.permissions import BasePermission
+
 
 class IsOwner(BasePermission):
     """
-       Custom permission to check if the user is the owner of the object.
-       """
+    Custom permission to check if the user is the owner of the object.
+    """
     def has_object_permission(self, request, view, obj):
         # Check if the user is the owner of the conversation or message
         return obj.owner == request.user
